d6918772c1dbe0cf4ca1a4a19e1ee02f73e6b228
Thibault Nocchi <thibaultnocchi@gmail.com>
2019-03-09 18:51:36 +0100

    Commented.

--- pytron.py
+++ pytron.py
@@ -10,21 +10,46 @@ from pyglet.font import Text
 # Grid
 class Grid:
 
-    def __init__(self, width, height):
+    def __init__(self, width: int, height: int):
         self.width = width
         self.height = height
         self.data = [[(0, 0)]*self.width for i in range(self.height)]
+        self.bonus = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21, 21)
 
-    def get_point(self, x, y):
+    def get_point(self, x: int, y: int) -> tuple:
+        """
+        Get the value associated to a coordinate into the grid.
+            :param self: 
+            :param x:int: X coord.
+            :param y:int: Y coord.
+        """   
         return self.data[y][x]
 
-    def set_point(self, x, y, value):
+    def set_point(self, x: int, y: int, value: tuple):
+        """
+        Set a value to a coordinate into the grid.
+            :param self: 
+            :param x:int: X coord.
+            :param y:int: Y coord.
+            :param value:tuple: Value to set.
+        """   
         self.data[y][x] = value
 
-    def reset_point(self, x, y):
+    def reset_point(self, x: int, y: int):
+        """
+        Reset the value associated to the coordinates.
+            :param self: 
+            :param x:int: X coord.
+            :param y:int: Y coord.
+        """   
         self.data[y][x] = (0, 0)
 
     def random_point(self):
+        """
+        Returns a tuple of random coordinates with nothing in it at this position.
+            :param self: 
+        """
         bx = randint(0, self.width - 1)
         by = randint(0, self.height - 1)
         for y in range(self.height):
@@ -36,8 +61,11 @@ class Grid:
         return None, None
 
     def show_bonus(self):
-        global bonus
-        b = choice(bonus)
+        """
+        Adds (or not) a bonus on the grid.
+            :param self: 
+        """   
+        b = choice(self.bonus)
         if b != 0:
             bx, by = self.random_point()
             self.set_point(bx, by, (b, 0))
@@ -337,8 +365,6 @@ for x in [10, 11, 78 - 18, 79 - 18]:
 #  for x in range(90):
 #    grid.set_point(x,y,(255,0))
 
-bonus = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 21, 21)
 cpu_ai = [
     (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 3),
